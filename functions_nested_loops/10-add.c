447635ba170a74fae9a27f0e2509d58c22828abc
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 21:46:12 +0000

    .

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Additionne deux entiers
+ * @a: premier entier
+ * @b: deuxieme entier
+ * Return: la somme de a et b 
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
