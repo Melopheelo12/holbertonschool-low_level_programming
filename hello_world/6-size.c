ea5feb72b3486e20296c9ebee753dac132842b89
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 13:46:13 +0000

    .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,30 +1,17 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
+ 
+main - Entry point*
+Return: Always 0*/
+
+int main (void)
 {
-    int n;
-    int last_digit;
-
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-
-    last_digit = n % 10;
-
-    printf("Last digit of %d is %d ", n, last_digit);
-
-    if (last_digit > 5)
-        printf("and is greater than 5\n");
-    else if (last_digit == 0)
-        printf("and is 0\n");
-    else
-        printf("and is less than 6 and not 0\n");
-
-    return (0);
+  int n;
+  srand(1);
+  n=rand()
+    printf("Last digit of %d is %d and is %s\n", n, n % 10,
+      n (n % 10 == 0) ? "0" : ((n % 10 > 5) ? "greater than 5" : "less than 6 and not 0"));
+  return(0);
 }
