d60e6c740128cf7b3a5214c5a6726ed1c26c722d
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 21:20:18 +0000

    .

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - Calcule la valueur absolue de d'un entier
+ * @n: L'entier a traiter
+ * Return: La valeur absolue de n
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
