f87216f176593d0d498afcd69261e24ce9eecf42
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 13:20:37 +0000

    .

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\\n");
+return (0);
+} ' > 5-printf.c
+
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c -o 5-printf
+./5-printf
+
