36950634e2d6f715e8e6541404ae6fec63b37f25
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 20:57:04 +0000

    .

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isalpha - Verifie si le caractere est une lettre
+ *@c: le caractere a verif (code ASCII)
+ *return: 1 si c'est une lettre, 0 sinon
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
