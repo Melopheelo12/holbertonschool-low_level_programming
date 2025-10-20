ae0dccea4a7ac4d897a70294e2804ea12a1e98db
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 13:51:08 +0000

    .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int n;
 
-	srand(1);
 	n = rand();
 	printf("Last digit of %d is %d and is %s\n", n, n % 10,
 	       (n % 10 == 0) ? "0" : ((n % 10 > 5) ? "greater than 5" : "less than 6 and not 0"));
