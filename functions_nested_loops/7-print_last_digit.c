4f420bce4d39ac6308eab3bb748cf10d08286e3e
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 21:25:29 +0000

    .

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_last_digit - Affiche le dernier chiffre d'un nombre
+ * @n: Le nombre a traiter
+ * Return: Le dernier chiffre de n
+ */
+int print_last_digit(int n)
+int print_last_digit(int n)
+{
+int last_digit;
+
+last_digit = n % 10;
+
+if (last_digit < 0)
+last_digit = -last_digit;
+
+_putchar(last_digit + '0');
+
+return (last_digit);
+}
