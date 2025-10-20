baee881f3e7556863cf43175b39c4a851df8cce1
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 12:23:44 +0000

    .

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
- int main(void)
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}' > 4-puts.c
-
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c -o 4-puts
-./4-puts
+    return 0;
+}
