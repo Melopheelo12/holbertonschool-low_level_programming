fe96a09402aba32044fe3f5676016a8466dfae4f
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 12:57:42 +0000

    .

--- hello_world/5-print.c
+++ hello_world/5-print.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\\n");
+return (0);
 }' > 5-printf.c
 
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c -o 5-printf
