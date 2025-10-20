4c8694bac3b8fab6a6ec7cb3ae46e9e6d0abd54f
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 20:51:00 +0000

    .

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_islower -Verif si un caractere est en min
- *@c: Le caractere a verifier (code ASCII) 
+ *@c: Le caractere a verifier (code ASCII)
  *Return: c'est 1 si c'est une min et 0 sinon
  */
 int _islower(int c)
