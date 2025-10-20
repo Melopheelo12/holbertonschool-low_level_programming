a250bc4500fb69fdea360cbe154ea0d7223f28d0
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 21:02:14 +0000

    .

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - Verifie si le caractere est une lettre (maj ou min)
- *@c: le caractere a verif (code ASCII)
- *return: 1 si c'est une lettre, 0 sinon
+ * _isalpha - Verifie si le caractere est une lettre (majuscule ou minuscule)
+ *@c: Le caractere a verifier (code ASCII)
+ * Return: 1 si c est une lettre, 0 sinon
  */
 int _isalpha(int c)
 {
