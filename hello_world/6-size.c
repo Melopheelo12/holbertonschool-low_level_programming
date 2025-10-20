88fc9988d576d0b5831066af996c2a91c8373bb4
Melopheelo12 <ryan12052002@gmail.com>
2025-10-20 13:48:43 +0000

    .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- 
-main - Entry point*
-Return: Always 0*/
-
-int main (void)
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-  int n;
-  srand(1);
-  n=rand()
-    printf("Last digit of %d is %d and is %s\n", n, n % 10,
-      n (n % 10 == 0) ? "0" : ((n % 10 > 5) ? "greater than 5" : "less than 6 and not 0"));
-  return(0);
+	int n;
+
+	srand(1);
+	n = rand();
+	printf("Last digit of %d is %d and is %s\n", n, n % 10,
+	       (n % 10 == 0) ? "0" : ((n % 10 > 5) ? "greater than 5" : "less than 6 and not 0"));
+	return (0);
 }
