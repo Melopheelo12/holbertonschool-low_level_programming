de09dd7504e4b7702771359ecf779fbb53becf53
Melopheelo12 <ryan12052002@gmail.com>
2025-10-22 15:15:27 +0000

    .

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -6,44 +6,44 @@
  */
 void print_times_table(int n)
 {
-    int i, j, res;
+int i, j, res;
 
-    if (n < 0 || n > 15)
-        return;
+if (n < 0 || n > 15)
+return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            res = i * j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+res = i * j;
 
-            if (j == 0)
-                _putchar(res + '0');
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (res < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(res + '0');
-                }
-                else if (res < 100)
-                {
-                    _putchar(' ');
-                    _putchar((res / 10) + '0');
-                    _putchar((res % 10) + '0');
-                }
-                else
-                {
-                    _putchar((res / 100) + '0');
-                    _putchar(((res / 10) % 10) + '0');
-                    _putchar((res % 10) + '0');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+if (j == 0)
+_putchar(res + '0');
+else
+{
+_putchar(',');
+_putchar(' ');
+if (res < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(res + '0');
+}
+else if (res < 100)
+{
+_putchar(' ');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar((res / 100) + '0');
+_putchar(((res / 10) % 10) + '0');
+_putchar((res % 10) + '0');
+}
+}
+}
+_putchar('\n');
+}
 }
 
