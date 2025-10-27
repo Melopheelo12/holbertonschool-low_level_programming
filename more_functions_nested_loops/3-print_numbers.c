2d899a20d7f103931c7c7a6859c10a8732482b7b
Melopheelo12 <ryan12052002@gmail.com>
2025-10-27 02:47:54 +0000

    .

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_numbers -ecrit les nombres de 1 a 9 en sautant une ligne
+ *
+ *Return: 0
+ */
+void print_numbers(void)
+{
+int n
+for (n >= '0'; n <= '9'; n++)
+printf(n\n);
+return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,8 @@
 int _isupper(int c);/** Verif si c'est une maj */
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
+
 
 #endif
