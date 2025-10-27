87d79c3abcf1d4b1173cd7b9ba712ede55d1f7e6
Melopheelo12 <ryan12052002@gmail.com>
2025-10-27 03:33:13 +0000

    .

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ for (i = 0; i < 10; i++)
 {
 for (c = 0; c <= 14; c++)
 {
-if (n >= 10)
+if (c >= 10)
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
