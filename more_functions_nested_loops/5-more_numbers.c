75a04a4b6634190ee07e8cc83c50eef007254229
Melopheelo12 <ryan12052002@gmail.com>
2025-10-27 03:31:08 +0000

    .

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *more_numbers -affiche les chiffre de 1 a 14 10 fois
+ */
+
+void more_numbers(void)
+{
+int i, c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 0; c <= 14; c++)
+{
+if (n >= 10)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+putchar('\n');
+}
+}
