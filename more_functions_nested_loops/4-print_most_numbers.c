72206ba0bf6e3794115b99b59c9e7aed44face2d
Melopheelo12 <ryan12052002@gmail.com>
2025-10-27 03:10:39 +0000

    .

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_most_numbers -ecrit les nimbre de 1 a 9 sauf 2 et 4
+ *
+ *Return: 0 succes
+ *
+ */
+
+Void print_most_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (c != '2' && c != '4')
+_putchar(c);
+}
+_putchar(\n);
+return (0);
+}
