237c70d2db113610f3286ec57b6a616fa8f241e2
Melopheelo12 <ryan12052002@gmail.com>
2025-10-27 16:36:34 +0000

    .

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *swap_int -swap les valeur de A et B
+ *@a: pointeur numero 1
+ *@b: pointeur numero 2
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+int z
+z = *a
+*a = *b
+*b = z
+}
