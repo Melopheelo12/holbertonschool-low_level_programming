cde81c4395ed5c2eef565068726954f36e33658d
Melopheelo12 <ryan12052002@gmail.com>
2025-10-27 03:40:02 +0000

    .

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_line -dessine une ligne
+ *
+ *@n: le nombre de '_'
+ *
+ */
+void print_line(int n)
+{
+int z;
+
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+for (z = 0; i < n; i++)
+_putchar('_');
+_putchar('\n');
+}
