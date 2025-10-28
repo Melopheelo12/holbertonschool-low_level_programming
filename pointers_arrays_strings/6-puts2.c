6e95872a3b7f51f413f0460ca21855ae365a0ea1
Melopheelo12 <ryan12052002@gmail.com>
2025-10-28 20:20:02 +0000

    .

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
+ *puts2 - affiche tout les cararteres du string
  *
- *
- *
+ *@str: pointeur du string
  *
  */
 void puts2(char *str)
 {
-  {
+{
 int i = 0;
 
- for (;str[i] != '\0'; i++)
+for (; str[i] != '\0'; i++)
 {
 if (i % 2 == 0)
 _putchar(str[i]);
