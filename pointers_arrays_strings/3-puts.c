94ea9bbdb3f40b1b3103f9898e4730cb91bb1289
Melopheelo12 <ryan12052002@gmail.com>
2025-10-28 08:40:26 +0000

    .

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_puts -Affiche une chaine 
+ *_puts -Affiche une chaine
  *@str: pointeur vers la chaine
  *
  *
@@ -8,7 +8,7 @@
 void _puts(char *str)
 {
 int i;
-for (i =0 ;str[i] != '\0'; i++)
+for (i = 0 ; str[i] != '\0'; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
