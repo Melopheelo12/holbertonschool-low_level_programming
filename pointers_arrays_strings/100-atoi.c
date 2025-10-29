ba9a5122817f8b8b95660628bffdb7fd24b0bfb6
Melopheelo12 <ryan12052002@gmail.com>
2025-10-29 12:35:50 +0000

    .

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 {
 int i = 0;
 int sign = 1;
-long long result = 0;
+int result = 0;
 int found_digit = 0;
 
 while (s[i] != '\0')
@@ -21,14 +21,11 @@ else if (s[i] >= '0' && s[i] <= '9')
 {
 found_digit = 1;
 result = result * 10 + (s[i] - '0');
-         
-if (sign == 1 && result > INT_MAX)
-return INT_MAX;
-if (sign == -1 && -result < INT_MIN)
-return INT_MIN;
+}
 else if (found_digit)
 break;
 i++;
 }
-return (int)(sign * result);
+
+return (result *sign);
 }
