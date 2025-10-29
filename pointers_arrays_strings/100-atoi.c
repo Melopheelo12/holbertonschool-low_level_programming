230e823b5ef116ebd31aa07674b0064e61f89890
Melopheelo12 <ryan12052002@gmail.com>
2025-10-29 12:43:40 +0000

    .

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -6,11 +7,12 @@
  *
  * Return: the integer value of the string, or 0 if no numbers found
  */
+
 int _atoi(char *s)
 {
 int i = 0;
 int sign = 1;
-int result = 0;
+long long result = 0;
 int found_digit = 0;
 
 while (s[i] != '\0')
@@ -21,11 +23,16 @@ else if (s[i] >= '0' && s[i] <= '9')
 {
 found_digit = 1;
 result = result * 10 + (s[i] - '0');
+
+if (sign == 1 && result > INT_MAX)
+return INT_MAX;
+if(sign == -1 && -result < INT_MIN)
+return INT_MIN;
 }
 else if (found_digit)
 break;
 i++;
 }
 
-return (result *sign);
+return (int)(sign * result);
 }
