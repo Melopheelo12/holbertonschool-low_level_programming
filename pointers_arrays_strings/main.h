16cc84c534f8a0723581d49574174f287fd32106
Melopheelo12 <ryan12052002@gmail.com>
2025-11-03 13:36:36 +0000

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -23,7 +23,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 
 
 #endif
