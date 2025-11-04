8e2259772d6747e422d608983a3bc3b67ea78c9a
Melopheelo12 <ryan12052002@gmail.com>
2025-11-04 11:22:22 +0000

    .

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion_helper(int n, int i)
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-		return(i);
+		return (i);
 	return (_sqrt_recursion_helper(n, i + 1));
 }
 /**
@@ -24,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion_helper(n, 0));	
+	return (_sqrt_recursion_helper(n, 0));
 }
