bc447809c06c9a45846df83e040008cb1c52526a
Melopheelo12 <ryan12052002@gmail.com>
2025-11-05 10:04:07 +0000

    .

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main -Entry point
+*@argc: count
+*@argv: caractere
+*Return: succes
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+		if (argc != 3)
+	{
+			printf("Error\n");
+			return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+
+	printf("%d\n", result);
+	return (0);
+}
