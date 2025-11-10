d57afe021c037bd8371e73426a988132df0122a7
Melopheelo12 <ryan12052002@gmail.com>
2025-11-10 15:02:51 +0000

    .

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -15,7 +15,8 @@ char *_strdup(char *str)
 	if (str == 0)
 	return (NULL);
 
-	for (; str[len] != '\0'; len++)
+	for (len = 0; str[len] != '\0'; len++)
+	;
 	dup = malloc((len + 1) * sizeof(char));
 	if (dup == 0)
 	return (NULL);
