179f8b2401eacfecae9b0e30e201dff5575402a5
Melopheelo12 <ryan1252002@gmail.com>
2025-11-13 11:10:52 +0000

    .

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*_calloc -allocates memory for an array
+*
+*@nmemb: nombre d'élément du tableau
+*@size: taille des élément du tableau
+*
+*Return: Pointeur vers la mémoire allouée initialisée à zéro, ou NULL en cas d’erreur
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total;
+	unsigned char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	total = nmemb * size;
+	p = ptr;
+	for (i = 0; i < total; i++)
+		p[i] = 0;
+
+	return (ptr);
+}
