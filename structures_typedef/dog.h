8787d459522dd2fc3b669d2691d34361caa291d6
Melopheelo12 <ryan12052002@gmail.com>
2025-11-14 11:46:30 +0000

    .

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure representing a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+typedef struct dog dog_t;
+
+#endif
