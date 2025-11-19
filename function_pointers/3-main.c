5a0abb034ec2b69b10353e47b4e47787385b146e
Melopheelo12 <ryan12052002@gmail.com>
2025-11-19 10:47:16 +0000

    .

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,3 +1,43 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, res;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+    {
+		printf("Error\n");
+		exit(99);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && j == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = func(i, j);
+	printf("%d\n", res);
+
+	return (0);
+}
