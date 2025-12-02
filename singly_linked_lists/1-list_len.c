aaaeac98de9f604cbf002c67cf006a373b06df62
Melopheelo12 <ryan12052002@gmail.com>
2025-12-02 12:37:12 +0000

    .

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - retourne le nombre d’éléments dans une liste list_t
+ * @h: pointeur constant vers le premier nœud (tête) de la liste
+ *
+ * Return: le nombre total de nœuds dans la liste
+ */
+size_t list_len(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
