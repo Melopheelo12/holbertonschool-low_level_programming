4e2a640e7113d67744f5ba5bc6e5dee72aee3327
Melopheelo12 <ryan12052002@gmail.com>
2025-12-05 01:33:07 +0000

    .

---
+++ file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - Creates a file and writes text to it
+ * @filename: Name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    ssize_t bytes_written;
+    size_t len = 0;
+
+    if (filename == NULL)
+        return -1;
+
+    if (text_content != NULL)
+    {
+        /* Compute the length of the text content */
+        while (text_content[len] != '\0')
+            len++;
+    }
+
+    /* Open file: create if it doesn't exist, truncate if it does, write only */
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return -1;
+
+    if (len > 0)
+    {
+        bytes_written = write(fd, text_content, len);
+        if (bytes_written != (ssize_t)len)
+        {
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+    return 1;
+}
