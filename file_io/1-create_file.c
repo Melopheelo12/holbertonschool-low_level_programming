71a155a120b38e4a1cf58eb2afd3a708c6360e41
Melopheelo12 <ryan12052002@gmail.com>
2025-12-05 01:53:15 +0000

    .

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,35 +13,35 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    size_t len = 0;
+int fd;
+ssize_t bytes_written;
+size_t len = 0;
 
-    if (filename == NULL)
-        return -1;
+if (filename == NULL)
+return -1;
 
-    if (text_content != NULL)
-    {
+if (text_content != NULL)
+{
 
-        while (text_content[len] != '\0')
-            len++;
-    }
+while (text_content[len] != '\0')
+len++;
+}
 
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return -1;
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return -1;
 
-    if (len > 0)
-    {
-        bytes_written = write(fd, text_content, len);
-        if (bytes_written != (ssize_t)len)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+if (len > 0)
+{
+bytes_written = write(fd, text_content, len);
+if (bytes_written != (ssize_t)len)
+{
+close(fd);
+return -1;
+}
+}
 
-    close(fd);
-    return 1;
+close(fd);
+return 1;
 }
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,38 +36,38 @@ int main(int argc, char *argv[])
     if (fd_from == -1)
         error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-    /* Open destination file for writing, create if not exists, truncate if exists */
-    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        close(fd_from);
-        error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	/* Open destination file for writing, create if not exists, truncate if exists */
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
     }
 
-    /* Copy the content in chunks of 1024 bytes */
-    while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+	/* Copy the content in chunks of 1024 bytes */
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
     {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-        {
-            close(fd_from);
-            close(fd_to);
-            error_exit(99, "Error: Can't write to %s\n", argv[2]);
-        }
-    }
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
 
-    if (bytes_read == -1)
-    {
-        close(fd_from);
-        close(fd_to);
-        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-    }
+	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
 
-    /* Close both file descriptors */
-    if (close(fd_from) == -1)
-        error_exit(100, "Error: Can't close fd %d\n", argv[1]);
-    if (close(fd_to) == -1)
-        error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+	/* Close both file descriptors */
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
 
-    return 0;
+	return 0;
 }
